5fd7b778ddd61bfa5c31b4c4102055ccb756f4f1
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2025-02-12 12:09:12 +0200

    feat: add WebVitals for dev

--- src/components/webVitals.tsx
+++ src/components/webVitals.tsx
@@ -2,12 +2,23 @@
  
 import { useReportWebVitals } from 'next/web-vitals'
  
+enum Rating { GOOD = 'good', NEEDS_IMPROVEMENT = 'needs-improvement', POOR = 'poor' }
+type RatingType = Rating.GOOD | Rating.NEEDS_IMPROVEMENT | Rating.POOR
+type Metric = { 
+    id: string; 
+    name: string; 
+    startTime: number; 
+    value: number; 
+    label: string; 
+    rating: RatingType;
+}
+
 export function WebVitals() {
-  useReportWebVitals((metric) => {
-    if(process.env.NODE_ENV === 'development' && metric.rating === 'good') {
-        console.info(metric);
+useReportWebVitals((metric: Metric) => {
+    if(process.env.NODE_ENV === 'development' && metric.rating !== Rating.GOOD) {
+            console.info('METRIC : ', metric);
     }
-  })
+})
 
   return <></>
 }
