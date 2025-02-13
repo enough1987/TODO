4ac98c5f12aaf086a291bb7924fab8a468e0e996
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2025-02-13 17:58:11 +0200

    feat: add Content Security Policy

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,37 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    webpackMemoryOptimizations: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
